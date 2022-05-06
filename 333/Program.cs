4a7ecf5423812010eb846e266a5c6f7e954061d4
Guo <1002076805@qq.com>
2022-05-06 18:34:03 +0300

    添加项目文件。

---
+++ 333/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squares_of_a_Sorted_Array
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            int[] nums = { 3, 2, 4, 6, 9 };
+
+            SortedSquares(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine("nums " + nums[i]);
+            }
+
+
+        }
+
+        public static int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] *= nums[i];
+            }
+
+            Array.Sort(nums);
+           
+            
+
+            return nums;
+        }
+
+
+
+
+
+
+    }
+
+}
